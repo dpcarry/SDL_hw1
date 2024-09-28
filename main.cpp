7ac489838d5c595bf04ce7a4834a52943cd5cfaa
Pingchuan Dong <131427529+dpcarry@users.noreply.github.com>
2024-09-28 19:01:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: [Your name here]
+* Assignment: Simple 2D Scene
+* Date due: 2023-09-20, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
